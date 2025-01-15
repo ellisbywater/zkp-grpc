f1f50ff450c47f2fdb6c8c3f61f54b7dcfe81c53
Ellis Garaudy <egaraudy@gmail.com>
2025-01-15 13:43:57 -0600

    server side authentication challenge handshake

--- src/server.rs
+++ src/server.rs
@@ -3,16 +3,18 @@ use std::sync::Mutex;
 use num_bigint::BigUint;
 use tonic::{transport::Server, Code, Request, Response, Status};
 
+use zkp::ZKP;
 pub mod zkp_auth {
     include!("./zkp_auth.rs");
 }
 
 use zkp_auth::{auth_server::{self, Auth, AuthServer}, AuthenticationAnswerResponse, AuthenticationChallengeRequest, RegisterRequest, RegisterResponse};
-
+use crate::zkp_auth::AuthenticationChallengeResponse;
 
 #[derive(Default)]
 pub struct AuthImpl {
-    pub user_info: Mutex<HashMap<String, UserInfo>>
+    pub user_info: Mutex<HashMap<String, UserInfo>>,
+    pub auth_to_user: Mutex<HashMap<String, String>>,
 }
 
 #[derive(Debug, Default)]
@@ -54,8 +56,25 @@ impl Auth for AuthImpl {
         Ok(Response::new(RegisterResponse {}))
     }
 
-    async fn create_authentication_challenge(&self, request: Request<AuthenticationChallengeRequest>) -> Result<Response<AuthenticationAnswerResponse>, Status> {
-        todo!()
+    async fn create_authentication_challenge(&self, request: Request<AuthenticationChallengeRequest>) -> Result<Response<AuthenticationChallengeResponse>, Status> {
+        let request = request.into_inner();
+        let username = request.user;
+
+        let mut user_info_map = self.user_info.lock().unwrap();
+        if let Some(user_info) = user_info_map.get_mut(&username) {
+            user_info.r1 = BigUint::from_bytes_be(&request.r1);
+            user_info.r2 = BigUint::from_bytes_be(&request.r2);
+
+            let (_,_,_,q) = ZKP::get_constants();
+            let c  = ZKP::generate_random_below(&q);
+            let auth_id = "skdjfsk".to_string();
+
+            let mut auth_to_user = self.auth_to_user.lock().unwrap();
+            auth_to_user.insert(auth_id.clone(), user_info.user_name.clone());
+            Ok(Response::new(AuthenticationChallengeResponse { auth_id, c: c.to_bytes_be()}))
+        } else {
+            Err(Status::unauthenticated("Username does not exist"))?
+        }
     }
 
     async fn verify_authentication(&self, request: Request<AuthenticationAnswerResponse>) -> Result<Response<AuthenticationAnswerResponse>, Status> {
